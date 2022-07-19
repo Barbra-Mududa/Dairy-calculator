85c3bcf35243199d6ba4a20ecdfb7475fc09df52
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-07-19 13:50:39 +0300

    restructure output section

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,13 @@ var sellingPrice = 45
 
 function sum(){
     shedA = Number(document.compute.g.value);
-    shedA = Number(document.compute.h.value);
-    shedA = Number(document.compute.i.value);
-    shedA = Number(document.compute.j.value);
+    shedB = Number(document.compute.h.value);
+    shedC = Number(document.compute.i.value);
+    shedD = Number(document.compute.j.value);
+
+    totalProductionD = g + h + i + j 
+
+    document.getElementById("daily").value = totalProductionD
 }
 
 
