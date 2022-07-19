3ff47eef9a1cd236c8760205ac68dafc82eb9b8d
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-07-19 13:41:00 +0300

    sum function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
-var shed = 
-
-
-
+var totalProductionD = g + h + i + j 
+var sellingPrice = 45 
 
 function sum(){
-    shedA
-    
+    shedA = Number(document.compute.g.value);
+    shedA = Number(document.compute.h.value);
+    shedA = Number(document.compute.i.value);
+    shedA = Number(document.compute.j.value);
 }
+
+
+
